d344c148960198ad8cde91fe18894f09a74cd03c
Gemius01 <pablo.gemio@inacapmail.cl>
2018-04-18 12:38:23 -0300

    asd

--- config.vue
+++ config.vue
@@ -1,6 +1,6 @@
 <script>
 	module.exports = {
-  API_LOCATION: 'http://localhost:8081',
+  API_LOCATION: 'http://localhost:8081aa',
   rules: [(v) => !!v || 'Campo vacío', v => (v && v.length <= 20) || 'Máximo de caracteres']
 }
 </script>
