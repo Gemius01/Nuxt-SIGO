bc8e70679553c819c496622d118cbfe762281052
Gemius01 <pablo.gemio@inacapmail.cl>
2018-04-18 12:54:09 -0300

    desactivado de eslint

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,6 @@ module.exports = {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
           exclude: /(node_modules)/
         })
       }
